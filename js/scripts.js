0f3fe12ca9d3fe47cef14b89d7d3d6c3ab0667e6
catherine bradley and julius bade <catherine@>
2018-08-09 09:49:48 -0700

    add a function to one branch to allow voting

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
+
   var age = parseInt(prompt("How old are you?"));
 
   if (age > 18) {
     $('#vote').show();
+    $("#ballot").click(function() {
+    var submit = $("input:radio[name=flavor]:checked").val();
+    alert(submit);
   } else if (age === 17) {
     $('#soonvote').show();
   } else {
     $('#novote').show();
   }
+
+  event.preventDefault()
+
+  });
 });
